2c6565a68454de3e59c8548a2c977bbb404bf187
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-08-04 21:19:41 -0300

    Conteudo tabela?

--- WebScraping/indicadores.py
+++ WebScraping/indicadores.py
@@ -1,3 +1,4 @@
+import pandas
 from selenium import webdriver
 from time import sleep
 from bs4 import BeautifulSoup
@@ -7,45 +8,63 @@ navegador = webdriver.Chrome()
 navegador.get("https://www2.aneel.gov.br/aplicacoes_liferay/srd/indqual/default.cfm")
 
 
-def atualiza_html_e_conta_options(tempo_sleep, nome_elemento):
+def atualiza_html(tempo_sleep):
     sleep(tempo_sleep)
-
     conteudo_web = BeautifulSoup(navegador.page_source, 'html.parser')
-    lista_E = conteudo_web.find('select', {'id': f'frm{nome_elemento}'})
+    return conteudo_web
+
+
+def conta_options(tempo_sleep, nome_elemento):
+    web_atualizado = atualiza_html(tempo_sleep)
+    lista_E = web_atualizado.find('select', {'id': f'frm{nome_elemento}'})
 
     qtd_E = int(len(lista_E) / 2)
     return qtd_E
 
 
-def seleciona_e_mostra_options(nome_elemento, nome_contador):
+def seleciona(nome_elemento, nome_contador):
     selecao = Select(navegador.find_element(f'xpath', f'//*[@id="frm{nome_elemento}"]'))
     selecao.select_by_index(nome_contador)
 
-    nomeE = navegador.find_element('xpath', f'//*[@id="frm{nome_elemento}"]/option[{nome_contador + 1}]')
-    print(nomeE.text)
 
+def cria_lista(nome_elemento):
+    web_atualizado = atualiza_html(2)
+    find_E = web_atualizado.find('select', {'id': f'frm{nome_elemento}'}).get_text()
+    E = find_E.split('\n')
+    print(E[2:-1])
+
+
+lista_estados = cria_lista('Estados')
 
 for e in range(1, 28):
-    seleciona_e_mostra_options('Estados', e)
+    seleciona('Estados', e)
 
-    qtd_municipios = atualiza_html_e_conta_options(2.5, 'Municipios')
-    print(qtd_municipios)
+    qtd_municipios = conta_options(2.5, 'Municipios')
+    # print(qtd_municipios)
+    lista_municipios = cria_lista('Municipios')
 
     for m in range(1, qtd_municipios):
-        seleciona_e_mostra_options('Municipios', m)
+        seleciona('Municipios', m)
 
-        qtd_anos = atualiza_html_e_conta_options(1, 'Anos')
-        print(qtd_anos)
+        qtd_anos = conta_options(1, 'Anos')
+        # print(qtd_anos)
+        lista_anos = cria_lista('Anos')
 
         for a in range(1, qtd_anos):
-            seleciona_e_mostra_options('Anos', a)
+            seleciona('Anos', a)
 
-            qtd_conjuntos = atualiza_html_e_conta_options(1, 'Conjuntos')
-            print(qtd_conjuntos)
+            qtd_conjuntos = conta_options(1, 'Conjuntos')
+            # print(qtd_conjuntos)
+            lista_conjuntos = cria_lista('Conjuntos')
 
             for c in range(1, qtd_conjuntos):
-                seleciona_e_mostra_options('Conjuntos', c)
+                seleciona('Conjuntos', c)
 
+                pd = pandas.DataFrame({'Estado': lista_estados[e+1],
+                                       'Município': lista_municipios[m+1],
+                                       'Ano': lista_anos[a+1],
+                                       'Conjunto': lista_conjuntos[c+1]})
+    print(pd)
     print('\n')
 
 navegador.quit()
