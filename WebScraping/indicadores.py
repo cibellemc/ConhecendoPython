a8e904bba687e9a17228714d2b4d1d6f93a178ae
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2022-08-25 11:43:20 -0300

    Tabelas auxiliares

--- WebScraping/indicadores.py
+++ WebScraping/indicadores.py
@@ -7,9 +7,7 @@ from selenium.webdriver.support.select import Select
 navegador = webdriver.Chrome()
 navegador.get("https://www2.aneel.gov.br/aplicacoes_liferay/srd/indqual/default.cfm")
 
-estmunano = []
-df_final = pd.DataFrame(columns=["ESTADO", "MUNICÍPIO", "ANO", "DIC ANUAL (horas)", "DIC ANUAL (horas)",
-"DIC ANUAL (horas)", "FIC ANUAL (interrupções)", "FIC ANUAL (interrupções)", "FIC ANUAL (interrupções)"])
+estmunanoconj = []
 
 
 def atualiza_html_e_conta_options(tempo_sleep, nome_elemento):
@@ -18,69 +16,59 @@ def atualiza_html_e_conta_options(tempo_sleep, nome_elemento):
     conteudo_web = BeautifulSoup(navegador.page_source, 'html.parser')
     lista_E = conteudo_web.find('select', {'id': f'frm{nome_elemento}'})
 
-    qtd_E = int(len(lista_E) / 2)
-    return qtd_E
+    return int(len(lista_E) / 2)
 
 
 def seleciona_elemento(nome_elemento, nome_contador):
-    selecao = Select(navegador.find_element(f'xpath', f'//*[@id="frm{nome_elemento}"]'))
+    selecao = Select(navegador.find_element('xpath', f'//*[@id="frm{nome_elemento}"]'))
     selecao.select_by_index(nome_contador)
 
 
 def cria_lista(nome_elemento):
     conteudo_web = BeautifulSoup(navegador.page_source, 'html.parser')
-    find_es = conteudo_web.find("select", {"id": f"frm{nome_elemento}"}).get_text()
-    estados = find_es.split('\n')
-    return estados
+    find_el = conteudo_web.find("select", {"id": f"frm{nome_elemento}"}).get_text()
+    return find_el.split('\n')
 
 
-def tables(num_table):
+def tables(indice_table):
     sleep(2)
-    table = navegador.find_element('xpath', f'//*[@id="resposta"]/table[{num_table}]')
-    html_element = table.get_attribute('outerHTML')
-    soup = BeautifulSoup(html_element, 'html.parser')
-    table = soup.find(name='table')
-    table_str = str(table)
-    df_aux = pd.DataFrame(pd.read_html(table_str.replace(',', '.')))
-    df_aux.drop(axis=0, inplace=True)
-    return df_aux[0]
+    web = BeautifulSoup(navegador.page_source, 'html.parser')
+    tabelas = web.find('div', {'id': 'resposta'}).find_all('tr', {'class': 'res'})
+
+    if indice_table == 1:
+        tabela1 = tabelas[0].text.strip().split("\n")
+    else:
+        tabela1 = tabelas[1].text.strip().split("\n")
+
+    df1 = pd.DataFrame(tabela1).replace(',', '.', regex=True).transpose()
+    sleep(2)
+
+    return df1
 
 
-for e in range(1, 28):
+for e in range(1, 2):
     seleciona_elemento('Estados', e)
-
     lista_estados = cria_lista('Estados')
-
     qtd_municipios = atualiza_html_e_conta_options(2.5, 'Municipios')
-    # print(qtd_municipios)
 
-    for m in range(1, qtd_municipios):
+    for m in range(1, 2):
         seleciona_elemento('Municipios', m)
-
         lista_municipios = cria_lista('Municipios')
-        # print(lista_municipios[m + 1])
-
         qtd_anos = atualiza_html_e_conta_options(1, 'Anos')
-        # print(qtd_anos)
 
-        for a in range(1, qtd_anos):
+        for a in range(1, 2):
             seleciona_elemento('Anos', a)
-
             lista_anos = cria_lista('Anos')
-            # print(lista_anos[a + 1])
-
             qtd_conjuntos = atualiza_html_e_conta_options(1.5, 'Conjuntos')
-            # print(qtd_conjuntos)
-            estmunano = [lista_estados[e+1], lista_municipios[m + 1].strip(), lista_anos[a+1]]
-            for c in range(1, qtd_conjuntos):
+
+            for c in range(1, 2):
                 seleciona_elemento('Conjuntos', c)
 
-                # lista_conjuntos = cria_lista('Conjuntos')
-                # print(lista_conjuntos[c + 1])
-                tabela1 = tables(1)
-                print(tabela1)
-                """tabela2 = tables(2)
-                print(tabela2[3] + tabela2[4])"""
-                df_final = df_final.append(tabela1)
-                print(df_final)
+                df1 = tables(1)
+                print(df1)
+
+                df2 = tables(2)
+                print(df2)
+
+
 navegador.quit()
