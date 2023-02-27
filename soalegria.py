e4169fa54bbc29318d6837c1ff9934960e17e965
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2023-02-27 17:37:20 -0300

    Update soalegria.py

--- soalegria.py
+++ soalegria.py
@@ -1,3 +1,9 @@
-with open("happy.txt", "w") as arquivo:
-    arquivo.write("Só alegria hahaha")
-    arquivo.close()
+try:
+    with open("happy.txt", "w") as arquivo:
+        arquivo.write("Só alegria hahaha")
+        arquivo.close()
+        print("Tudo na vida tem vantagens e desvantagens")
+except:
+        print("Algo deu errado")
+
+
