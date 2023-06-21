449813aac771a7fba24275f05c7648a3ec60ea4c
Cibelle Maciel <108502128+cibellemc@users.noreply.github.com>
2023-06-21 09:54:02 -0300

    add: mecanismo para simular interação humana

--- webscraping-funcional/extracao_mensal.py
+++ webscraping-funcional/extracao_mensal.py
@@ -1,3 +1,5 @@
+from time import sleep
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -9,7 +11,7 @@ from selenium.webdriver.chrome.options import Options
 def EMC (ano_inicial_analise, ano_final_analise, numero_tentativas, distribuidoras, tipo_extracao):
     # configurações pra não abrir o browser
     hidden_browser = Options()
-    hidden_browser.headless = True
+    hidden_browser.headless = False
     driver = webdriver.Chrome(options=hidden_browser)
 
     # inicializa o log de registro
@@ -38,10 +40,18 @@ def EMC (ano_inicial_analise, ano_final_analise, numero_tentativas, distribuidor
             ano_final = ano_final_analise
 
         for ano in range(ano_inicial, ano_final + 1):
-            for mes in range(1 , 12 + 1):
+            # ajustar de acordo com mês de referência
+            for mes in range(4 , 4 + 1):
+
+                hidden_browser = Options()
+                # necessário deixar tela aberta para funcionar
+                hidden_browser.headless = False
+                driver = webdriver.Chrome(options=hidden_browser)
+
                 for tentativa in range(1, numero_tentativas + 1):
-                    url = 'https://www2.aneel.gov.br/aplicacoes/indicadores_de_qualidade/DecFecSegMensal.cfm?mes={}&ano={}&regiao={}&distribuidora={}&tipo=d'.format(mes, ano, regiao, id_distribuidora)
+
                     try:
+                        url = 'https://www2.aneel.gov.br/aplicacoes/indicadores_de_qualidade/DecFecSegMensal.cfm?mes={}&ano={}&regiao={}&distribuidora={}&tipo=d'.format(mes, ano, regiao, id_distribuidora)
                         driver.get(url)
 
                         element = driver.find_element(By.XPATH, "//table[@class='tab_indice']")
@@ -50,7 +60,6 @@ def EMC (ano_inicial_analise, ano_final_analise, numero_tentativas, distribuidor
                         table = soup.find(name='table')
                         table_str = str(table)
 
-                        df_aux = pd.DataFrame()
                         df_aux = (pd.read_html(table_str.replace(',', '~'))[0]).replace('~', ',', regex = True)
                         df_aux.drop(df_aux.head(3).index, inplace=True)
                         df_aux.drop(df_aux.tail(3).index, inplace=True)
@@ -62,6 +71,7 @@ def EMC (ano_inicial_analise, ano_final_analise, numero_tentativas, distribuidor
 
                         print('{};{};{}/{};tentativa {};ok'.format(tipo_extracao, sigla_distribuidora, nome_meses[int(mes - 1)], ano, tentativa))
                         log.write(str(('{};{};{}/{};tentativa {};ok\n'.format(tipo_extracao, sigla_distribuidora, nome_meses[int(mes - 1)], ano, tentativa))))
+
                         break
                     except:
                         if tentativa == numero_tentativas:
@@ -69,12 +79,15 @@ def EMC (ano_inicial_analise, ano_final_analise, numero_tentativas, distribuidor
                             log.write(str(('{};{};{}/{};tentativa {};erro\n'.format(tipo_extracao, sigla_distribuidora, nome_meses[int(mes - 1)], ano, tentativa))))
                         else:
                             pass
+                
+            driver.close()
+            
 
     df.columns = ['ID DISTRIBUIDORA', 'PERIODO', 'CONJUNTO',  'COD CONJUNTO', 'NUMERO UCS', 'DECTOT', 'FECTOT',  'DECXP', 'FECXP', 'DECXN', 'FECXN', 'DECIP', 'FECIP', 'DECIND', 'FECIND', 'DECINE', 'FECINE', 'DECINC', 'FECINC', 'DECINO', 'FECINO', 'DECIPC', 'FECIPC', 'DECXPC', 'FECXPC', 'DECXNC', 'FECXNC']
 
     df = df.astype(str)
     df = df.replace('nan', '', regex=True)
 
-    df.to_csv('base' + tipo_extracao + '.txt', sep=';', index=False, encoding='utf-8-sig')
+    df.to_csv('resultados/base' + tipo_extracao + '.txt', sep=';', index=False, encoding='utf-8-sig')
 
     log.close()
